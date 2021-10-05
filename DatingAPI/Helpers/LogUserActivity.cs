c42790ae7b1421448fcab93e70e4feea5bd9bf35
perfectimhetsike.igbadumhe@stanbicibtc.com <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-10-06 00:38:34 +0100

    used action filter to update last active date

---
+++ DatingAPI/Helpers/LogUserActivity.cs
@@ -0,0 +1,27 @@
+﻿using DatingAPI.Extensions;
+using DatingAPI.Interface;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatingAPI.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>(); //import using Microsoft.Extensions.DependencyInjection for getservices to work
+            var user = await repo.GetUserByUsername(username);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
